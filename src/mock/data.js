7f16a4ac83a74fba55501c3f01e741e397cb5a6c
sabzehparvar <sabzehparvar.mojtaba@gmail.com>
2022-05-04 13:43:04 +0430

    minor change

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: 'I am a passionate junior front-end developer with experience building frontend applications using React, JavaScript, jQuery, HTML, and CSS. I enjoy the creative and logical aspects that come with creating websites and I love learning new things every day.',
   paragraphTwo: ' ',
   paragraphThree: ' ',
-  resume: 'https://www.dropbox.com/s/iw0oteghlkmlyz5/cv.pdf?dl=0', // if no resume, the button will not show up
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
